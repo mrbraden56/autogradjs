7220d4faa282e3e9c4fd4edc45aeab7fb5fa904c
Braden Lockwood <braden.lockwood@kiewit.com>
2024-05-20 11:35:24 -0400

    autogradjs almost done except for bug where weights are 0 when I go to apply sgd

--- AutoJs.js
+++ AutoJs.js
@@ -3,7 +3,7 @@ function randomUniform(min, max) {
 }
 
 class Tensor {
-  constructor(value, parents, grad, backward) {
+  constructor(value, parents = [], grad = 0, backward = null) {
     this.value = value;
     this.parents = parents;
     this.grad = grad;
@@ -12,13 +12,13 @@ class Tensor {
 
   static add(x1, x2) {
     if (!(x1 instanceof Tensor)) {
-      x1 = new Tensor(x1, null, 0);
+      x1 = new Tensor(x1);
     }
     if (!(x2 instanceof Tensor)) {
-      x2 = new Tensor(x2, null, 0);
+      x2 = new Tensor(x2);
     }
 
-    var y = new Tensor(x1.value + x2.value, [x1, x2], 0);
+    var y = new Tensor(x1.value + x2.value, [x1, x2]);
     var _backward = function _backward() {
       x1.grad += 1 * y.grad;
       x2.grad += 1 * y.grad;
@@ -26,15 +26,33 @@ class Tensor {
     y._backward = _backward;
     return y;
   }
-  static mul(x1, x2) {
+
+  static sub(x1, x2) {
     if (!(x1 instanceof Tensor)) {
-      x1 = new Tensor(x1, null, 0);
+      x1 = new Tensor(x1);
     }
     if (!(x2 instanceof Tensor)) {
-      x2 = new Tensor(x2, null, 0);
+      x2 = new Tensor(x2);
     }
 
-    var y = new Tensor(x1.value * x2.value, [x1, x2], 0);
+    var y = new Tensor(x1.value - x2.value, [x1, x2]);
+    var _backward = function _backward() {
+      x1.grad += 1 * y.grad;
+      x2.grad += 1 * y.grad;
+    };
+    y._backward = _backward;
+    return y;
+  }
+
+  static mul(x1, x2) {
+    if (!(x1 instanceof Tensor)) {
+      x1 = new Tensor(x1);
+    }
+    if (!(x2 instanceof Tensor)) {
+      x2 = new Tensor(x2);
+    }
+
+    var y = new Tensor(x1.value * x2.value, [x1, x2]);
     var _backward = function _backward() {
       x1.grad += x2.value * y.grad;
       x2.grad += x1.value * y.grad;
@@ -43,51 +61,94 @@ class Tensor {
     return y;
   }
 
+  static pow(x1, power) {
+    if (!(x1 instanceof Tensor)) {
+      x1 = new Tensor(x1);
+    }
+
+    var y = new Tensor(x1.value ** power, [x1]);
+    var _backward = function _backward() {
+      x1.grad += power * y.value ** (power - 1) * y.grad;
+    };
+    y._backward = _backward;
+    return y;
+  }
+
+  static relu(x) {
+    var val = x.value;
+    if (val < 0) {
+      val = 0;
+    }
+    var y = new Tensor(val);
+    var _backward = function _backward() {
+      this.grad += (y.value > 0) * y.grad;
+    };
+    y._backward = _backward;
+    return y;
+  }
+
   static array(x) {
     for (var i = 0; i < x.length; i++) {
       for (var j = 0; j < x[0].length; j++) {
-        x[i][j] = new Tensor(x[i][j], null, 0);
+        x[i][j] = new Tensor(x[i][j]);
       }
     }
     return x;
   }
 
-  backward() {}
+  backward() {
+    var topo = [];
+    var visited = new Set();
+
+    function build_topo(v) {
+      if (!visited.has(v)) {
+        visited.add(v);
+        if (v.parents) {
+          for (const child of v.parents) {
+            build_topo(child);
+          }
+        }
+        topo.push(v);
+      }
+    }
+
+    build_topo(this);
+    this.grad = 1;
+    topo.reverse();
+
+    for (const v of topo) {
+      if (v._backward) {
+        v._backward();
+      }
+    }
+  }
 }
 
 class Matrix {
-  //NOTE: This class operates on the Tensor class
-
   zeroes(r, c, val, generator) {
     var matrix = [];
     for (var i = 0; i < r; i++) {
       var row = [];
       for (var j = 0; j < c; j++) {
         if (generator) {
-          row.push(val(-1, 1));
+          var ten_val = new Tensor(val(-1, 1));
+          row.push(ten_val);
         } else {
-          row.push(val);
+          var ten_val = new Tensor(val);
+          row.push(ten_val);
         }
       }
-
       matrix.push(row);
     }
     return matrix;
   }
+
   static matmul(a, b) {
-    var m = a.length; // Number of rows in A
-    var k = a[0].length; // Number of columns in A
-    var n = b[0].length; // Number of columns in B
+    var m = a.length;
+    var k = a[0].length;
+    var n = b[0].length;
 
-    var ldA = k; // Leading dimension of A (number of columns)
-    var ldB = n; // Leading dimension of B (number of columns)
-    var ldC = n; // Leading dimension of C (number of columns)
-
-    var rows = m; // Number of rows in C (same as A)
-    var columns = n; // Number of columns in C (same as B)
-
-    // Create a zero matrix to store the result
-    var c = new Matrix().zeroes(rows, columns, 0, false);
+    var c = new Matrix().zeroes(m, n, 0, false);
 
     for (var i = 0; i < m; i++) {
       for (var j = 0; j < n; j++) {
@@ -95,6 +156,8 @@ class Matrix {
           var product = Tensor.mul(a[i][p], b[p][j]);
           c[i][j] = Tensor.add(c[i][j], product);
         }
+        // Apply ReLU activation function
+        c[i][j] = Tensor.relu(c[i][j]);
       }
     }
     return c;
@@ -105,6 +168,7 @@ class Layer {
   constructor(nin, nout) {
     this.weights = new Matrix().zeroes(nin, nout, randomUniform, true);
   }
+
   forward(x) {
     return Matrix.matmul(x, this.weights);
   }
@@ -112,7 +176,7 @@ class Layer {
 
 class FFN {
   constructor() {
-    this.layers = this.layers = [
+    this.layers = [
       new Layer(10, 32),
       new Layer(32, 64),
       new Layer(64, 32),
@@ -124,35 +188,73 @@ class FFN {
     for (var i = 0; i < this.layers.length; i++) {
       x = this.layers[i].forward(x);
     }
+
     return x;
+  }
+
+  zero_grad() {
+    for (var i = 0; i < this.layers.length; i++) {
+      var r = this.layers[i].weights.length;
+      var c = this.layers[i].weights[0].length;
+      for (var j = 0; j < r; j++) {
+        for (var k = 0; k < c; k++) {
+          this.layers[i].weights[j][k].grad = 0;
+        }
+      }
+    }
+  }
+
+  sgd(step) {
+    for (var i = 0; i < this.layers.length; i++) {
+      var r = this.layers[i].weights.length;
+      var c = this.layers[i].weights[0].length;
+      for (var j = 0; j < r; j++) {
+        for (var k = 0; k < c; k++) {
+          console.log("here")
+          this.layers[i].weights[j][k].value +=
+            -this.layers[i].weights[j][k].grad * step;
+          console.log("here")
+        }
+      }
+    }
   }
 }
 
 if (require.main === module) {
-  x = [
+  var x = [
     [-2, 1, 0, -1, 2, 1, -1, 2, -2, 1],
     [1, -1, 2, -2, 1, 0, 2, -1, 0, -3],
     [0, -2, 1, -1, 2, -1, 0, -1, 1, -3],
     [-1, 2, -1, 1, -2, 0, -1, 2, 0, 1],
     [2, -1, 1, -2, 1, 2, -1, 0, 1, -1],
     [0, 1, -2, 2, -1, 1, -1, 0, 2, -1],
-  ]; //NOTE: 6X10
+  ];
+  x = Tensor.array(x);
 
-  var x = Tensor.array(x);
-  nn = new FFN();
+  var ys = [[1, -2, 2, -1, 1, -2]];
+  ys = Tensor.array(ys);
+  var nn = new FFN();
 
   var epochs = 100;
   for (var i = 0; i < epochs; i++) {
-    //nn.zero_grad();
-    out = nn.forward(x);
-    //loss
-    //loss.backward()
-    //sgd 0.01
-    if (i % 10 === 0) {
-      console.log(`Epoch: ${i}`);
+    nn.zero_grad(0.01);
+    var out = nn.forward(x);
+    var loss = new Tensor(0);
+
+    for (var j = 0; j < out.length; j++) {
+      var difference = Tensor.sub(out[j][0], ys[0][j]);
+      var mse = Tensor.pow(difference, 2);
+      loss = Tensor.add(loss, mse);
+    }
+    loss.backward();
+    nn.sgd(0.01);
+    if (i % 1 === 0) {
+      console.log(`Epoch: ${i}, Loss: ${loss.value}`);
     }
   }
-  if (i % 10 === 0) {
-    console.log(`Epoch: ${i}`);
-  }
+  console.log(`Epoch: ${epochs}, Loss: ${loss.value}`);
 }
+
+//NOTE: TODO
+//I think the loss is staying the same because backward() isnt updating the gradients
+//Still missing Bias
